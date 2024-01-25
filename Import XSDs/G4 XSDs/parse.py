1f7572e705475cff3dfcbf55a2460e6a087b2ec2
Cina Arjmand <116003645+CinaArjmand@users.noreply.github.com>
2024-01-25 14:59:48 +0100

    jpnb created

--- Import XSDs/G4 XSDs/parse.py
+++ Import XSDs/G4 XSDs/parse.py
@@ -1,3 +1,17 @@
 import xmlschema
 from pprint import pprint
-schema = xmlschema.XMLSchema('./G4_DMS_v0.1.xsd')
+# import pandas as pd
+# data = pd.read_excel('../../Clarifications/G4 - mandatory data elements 11.10.2023 (1) - Copy.xlsx')
+# print(data.keys())
+
+# elements = data.at[1, 'Data element/class name ']
+# print(elements)
+
+# xxx
+
+
+schema_file = open('./G4_DMS_v0.1_copy.xsd')
+schema = xmlschema.XMLSchema(schema_file)
+pprint(schema.annotations)
+# for xsd_component in schema.iter_components():
+    # pprint(xsd_component)
